cc95b6cd31301d2db3d15974e0a0df37c26b1101
Swanandi Bhende <swanandibhende@gmail.com>
2025-06-27 17:41:07 +0530

    270625_1741

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ print("hello, world!")
 print("我说中文")
 name= input("enter your name: ")
 print("your name is: ", name)
+# the end is default set to \n for a new line, so you can put anything you want in there 
+print("another way: ", end="")
+print(name)
+print("After multiple trials and errors, I am once again giving a \"second chance\" to learning \'python\' again")
+print(f"你好 fine shyt, {name}")
